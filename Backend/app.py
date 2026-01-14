d7ed32514bd23c3dbc977db606d9fbfe703ed87a
SuhasBennur <suhas.bennur@gmail.com>
2026-01-14 15:37:41 +0530

    Fixed naming ambiguities

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,14 @@
 from Core.Config import create_app
 from Core.Excel_Utils import init_excel
-from Routes import history, analytics
+from Routez import History, Analytics
 from Sockets.Oil_Stream import oil_stream
 
 app = create_app()
 init_excel()
 
 # Include routers
-app.include_router(history.router)
-app.include_router(analytics.router)
+app.include_router(History.router)
+app.include_router(Analytics.router)
 
 # WebSocket route
 app.websocket("/ws/oil")(oil_stream)
